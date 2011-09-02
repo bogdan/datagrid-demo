5825ab7bfac0084192b360b0299f0e78fae3b192
Bogdan Gusiev <agresso@gmail.com>
2011-09-02 15:56:43 +0300

    Datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,9 +6,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require date
+//= require date-format
 //= require_tree .
 //
 
+Date.format = "yyyy-mm-dd"
 $(document).ready(function() {
   sh_highlightDocument();
+  $('.date_filter').datePicker({startDate: '2010-01-01'});
+  $("#dp-popup").addClass("dp-popup")
 })
--- app/controllers/time_entry_reports_controller.rb
+++ app/controllers/time_entry_reports_controller.rb
@@ -2,5 +2,6 @@ class TimeEntryReportsController < ApplicationController
 
   def index
     @time_entry_report = TimeEntryReport.new(params[:time_entry_report])
+    @assets = @time_entry_report.assets.paginate(:page => params[:page])
   end
 end
