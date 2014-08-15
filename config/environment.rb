b3a5684eb3fa23b9d52249de6157573828557486
Bogdan Gusiev <agresso@gmail.com>
2014-08-15 09:29:08 +0300

    do

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,9 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+path = Rails.root.join('config/mongoid.yml')
+puts path
+environment = defined?(Rails) && Rails.respond_to?(:env) ? Rails.env : ENV["RACK_ENV"]
+settings = YAML.load(ERB.new(File.new(path).read).result)[environment]
+puts settings.to_yaml
 DatagridDemo::Application.initialize!
