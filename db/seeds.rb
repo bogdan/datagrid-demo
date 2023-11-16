8ba911851b7a724aff87520d0f4456269fa96f85
Bogdan Gusiev <agresso@gmail.com>
2023-11-16 11:39:41 +0200

    Allow db cleanup in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'database_cleaner/active_record'
 DatabaseCleaner.clean_with(:truncation)
+DatabaseCleaner.allow_production = true
+DatabaseCleaner.allow_remote_database_url = true
 
 Project.transaction do
   ["Google", "Apple", "Microsoft"].each do |name|
